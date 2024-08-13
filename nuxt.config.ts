e3adc4a29b59b99ce9a2628d402f91a615137539
Romain <romain.gilles@360learning.com>
2024-08-13 14:39:30 +0200

    add a route explicitely

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   nitro: {
     prerender: {
-        crawlLinks: false
+        crawlLinks: false,
+        routes: ['/other'],
     }
   }
 })
